30d4418b10c53ad5777ec3096155b19d5447eed3
sswierczek <sswierczek@wi.zut.edu.pl>
2012-10-30 22:56:21 +0100

    Minor changes

--- src/pl/edu/zut/mad/tools/inclinometer/Inclinometer.java
+++ src/pl/edu/zut/mad/tools/inclinometer/Inclinometer.java
@@ -1,5 +1,6 @@
 package pl.edu.zut.mad.tools.inclinometer;
 
+import pl.edu.zut.mad.tools.R;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -15,85 +16,85 @@ import android.widget.Toast;
 
 public class Inclinometer extends Activity implements SensorEventListener {
 
-    private SensorManager sensor_manager;
-    private boolean color = false;
-    private View view;
-    private long lastUpdate;
+	private SensorManager sensor_manager;
+	private boolean color = false;
+	private View view;
+	private long lastUpdate;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 
-	requestWindowFeature(Window.FEATURE_NO_TITLE);
-	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.imeter);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.imeter);
 
-	view = findViewById(R.id.textView);
-	view.setBackgroundColor(Color.GREEN);
-
-	sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-	lastUpdate = System.currentTimeMillis();
-    }
-
-    @Override
-    protected void onPause() {
-	super.onPause();
-	sensor_manager.unregisterListener(this);
-    }
-
-    @Override
-    protected void onResume() {
-	super.onResume();
-	// register this class as a listener for the orientation and
-	// accelerometer sensors
-	sensor_manager.registerListener(this,
-		sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-		sensor_manager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	getMenuInflater().inflate(R.menu.imeter, menu);
-	return true;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-	if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-	    getAccelerometer(event);
-	}
-    }
-
-    private void getAccelerometer(SensorEvent event) {
-	float[] values = event.values;
-
-	float x = values[0];
-	float y = values[1];
-	float z = values[2];
-
-	float accelationSquareRoot = (x * x + y * y + z * z)
-		/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-	long actualTime = System.currentTimeMillis();
-	if (accelationSquareRoot >= 2) //
-	{
-	    if (actualTime - lastUpdate < 200) {
-		return;
-	    }
-	    lastUpdate = actualTime;
-	    Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
-		    .show();
-	    if (color) {
+		view = findViewById(R.id.textView);
 		view.setBackgroundColor(Color.GREEN);
 
-	    } else {
-		view.setBackgroundColor(Color.RED);
-	    }
-	    color = !color;
+		sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		lastUpdate = System.currentTimeMillis();
 	}
-    }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensor_manager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// register this class as a listener for the orientation and
+		// accelerometer sensors
+		sensor_manager.registerListener(this,
+				sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				sensor_manager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.imeter, menu);
+		return true;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			getAccelerometer(event);
+		}
+	}
+
+	private void getAccelerometer(SensorEvent event) {
+		float[] values = event.values;
+
+		float x = values[0];
+		float y = values[1];
+		float z = values[2];
+
+		float accelationSquareRoot = (x * x + y * y + z * z)
+				/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+		long actualTime = System.currentTimeMillis();
+		if (accelationSquareRoot >= 2) //
+		{
+			if (actualTime - lastUpdate < 200) {
+				return;
+			}
+			lastUpdate = actualTime;
+			Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
+					.show();
+			if (color) {
+				view.setBackgroundColor(Color.GREEN);
+
+			} else {
+				view.setBackgroundColor(Color.RED);
+			}
+			color = !color;
+		}
+	}
 }
