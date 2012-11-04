afe77a19bb609af6a03fd3e608705b229399b43b
Dawid Glinski <dawidg90@gmail.com>
2012-11-04 15:26:04 +0100

    added logic for inclinometer class

--- src/pl/edu/zut/mad/tools/MainActivity.java
+++ src/pl/edu/zut/mad/tools/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.edu.zut.mad.tools;
 
+import pl.edu.zut.mad.tools.inclinometer.Inclinometer;
 import pl.edu.zut.mad.tools.nanny.Nanny;
 import android.os.Bundle;
 import android.app.Activity;
@@ -11,33 +12,42 @@ import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	// TESTING BUTTONS
-	Button btnNanny;
+    // TESTING BUTTONS
+    Button btnNanny;
+    Button btnInclinometer;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 
-		btnNanny = (Button) findViewById(R.id.btnNanny);
-		btnNanny.setOnClickListener(this);
+	btnNanny = (Button) findViewById(R.id.btnNanny);
+	btnNanny.setOnClickListener(this);
+
+	btnInclinometer = (Button) findViewById(R.id.btnInclinometer);
+	btnInclinometer.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	getMenuInflater().inflate(R.menu.activity_main, menu);
+	return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+	switch (v.getId()) {
+	case R.id.btnNanny:
+	    Intent nannyIntent = new Intent(this, Nanny.class);
+	    startActivity(nannyIntent);
+	    break;
+
+	case R.id.btnInclinometer:
+	    Intent inclinometerIntent = new Intent(this, Inclinometer.class);
+	    startActivity(inclinometerIntent);
+	    break;
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btnNanny:
-			Intent nannyIntent = new Intent(this, Nanny.class);
-			startActivity(nannyIntent);
-			break;
-
-		}
-
-	}
+    }
 }
--- src/pl/edu/zut/mad/tools/inclinometer/Inclinometer.java
+++ src/pl/edu/zut/mad/tools/inclinometer/Inclinometer.java
@@ -1,7 +1,6 @@
 package pl.edu.zut.mad.tools.inclinometer;
 
 import pl.edu.zut.mad.tools.R;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,92 +8,89 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class Inclinometer extends Activity implements SensorEventListener {
 
-	private SensorManager sensor_manager;
-	private boolean color = false;
-	private View view;
-	private long lastUpdate;
+    private TextView xPostv, yPostv, zPostv;
+    private SensorManager sensor_manager;
+    private long lastUpdate;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.imeter);
+	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_inclinometer);
 
-		view = findViewById(R.id.textView);
-		view.setBackgroundColor(Color.GREEN);
+	xPostv = (TextView) findViewById(R.id.xPostv);
+	yPostv = (TextView) findViewById(R.id.yPostv);
+	zPostv = (TextView) findViewById(R.id.zPostv);
 
-		sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		lastUpdate = System.currentTimeMillis();
+	sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause() {
+	super.onPause();
+	sensor_manager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+	super.onResume();
+	// register this class as a listener for the orientation and
+	// accelerometer sensors
+	sensor_manager.registerListener(this,
+		sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+		sensor_manager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	getMenuInflater().inflate(R.menu.activity_inclinometer, menu);
+	return true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+	if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+	    getAccelerometer(event);
 	}
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		sensor_manager.unregisterListener(this);
+    private void getAccelerometer(SensorEvent event) {
+	float[] values = event.values;
+
+	float x = values[0];
+	float y = values[1];
+	float z = values[2];
+
+	float accelationSquareRoot = (x * x + y * y + z * z)
+		/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+
+	long actualTime = System.currentTimeMillis();
+
+	if (accelationSquareRoot <= 0.6) //
+	{
+	    xPostv.setText(String.format("%.2f", x));
+	    yPostv.setText(String.format("%.2f", y));
+	    zPostv.setText(String.format("%.2f", z));
+	    
+	    if (actualTime - lastUpdate < 200) {
+		return;
+	    }
+	    lastUpdate = actualTime;
+
 	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		// register this class as a listener for the orientation and
-		// accelerometer sensors
-		sensor_manager.registerListener(this,
-				sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				sensor_manager.SENSOR_DELAY_NORMAL);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.imeter, menu);
-		return true;
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			getAccelerometer(event);
-		}
-	}
-
-	private void getAccelerometer(SensorEvent event) {
-		float[] values = event.values;
-
-		float x = values[0];
-		float y = values[1];
-		float z = values[2];
-
-		float accelationSquareRoot = (x * x + y * y + z * z)
-				/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-		long actualTime = System.currentTimeMillis();
-		if (accelationSquareRoot >= 2) //
-		{
-			if (actualTime - lastUpdate < 200) {
-				return;
-			}
-			lastUpdate = actualTime;
-			Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
-					.show();
-			if (color) {
-				view.setBackgroundColor(Color.GREEN);
-
-			} else {
-				view.setBackgroundColor(Color.RED);
-			}
-			color = !color;
-		}
-	}
+    }
 }
