b3142bb8e041b9d8c39a20d1ff0e53275814dc0c
Michazzz <michal-sajmon@wp.pl>
2012-11-20 15:32:14 +0100

    Changes in WhereIsCar Class
    GPS location in TextView all time and it is working in "real time".
    Saved location is confrimed by Toats Object.

---
+++ src/pl/edu/zut/mad/tools/utils/Constans.java
@@ -0,0 +1,9 @@
+package pl.edu.zut.mad.tools.utils;
+
+public class Constans {
+	
+    /** Stala definiujaca nazwe pliku ustawien */
+    public static final String GPS_LOCATION_PREFERENCES = "GPS_Location_Preferences";
+    
+    public static final String PREFERENCES_NAME = "MAD_TOOLS_Preferences";
+}
